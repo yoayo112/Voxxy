52e582a814cc509e54d3670c1a496500d2b0bcb1
Sky <yoayo112@gmail.com>
2025-10-24 14:57:37 -0600

    added reference pitch

--- VOX/setrange.tsx
+++ VOX/setrange.tsx
@@ -13,7 +13,7 @@
 // - if failed twice in a row- -> set pitch as range!! 
 // Some Kind of visual?? 
 
-import React , {useState, useEffect} from 'react';
+import React , {useState, useEffect, useCallback} from 'react';
 import { 
   SafeAreaView, 
   Text, 
@@ -27,7 +27,8 @@ import { Profile } from './profile';
 import styles from './UI/styles';
 import BackButton from './UI/backButton';
 
-const Sound = require('react-native-sound');
+import Sound from 'react-native-sound';
+//const Sound = require('react-native-sound');
 
  interface setRangeScreenProps {
   onBack: () => void;
@@ -35,7 +36,7 @@ const Sound = require('react-native-sound');
 
 
  //pitch match screen
-const SetRangeScreen: React.FC<setRangeScreenProps> = ({ onBack }) => {
+const SetRangeScreen: React.FC<setRangeScreenProps> = ({ onBack }) => { 
   
   const startPitch = Pitches.C4;
   const lowPitch = Pitches.C4;
@@ -46,28 +47,109 @@ const SetRangeScreen: React.FC<setRangeScreenProps> = ({ onBack }) => {
     const [low_max, setLow_max] = useState("C4");
     const [high_max, setHigh_max] = useState("C4");
     const [expected, setExpected] = useState(Pitches.C4.frequency);
-    const [grade, setGrade] = useState(1.0); // TODO this needs to be scored per "slice" 
+    const [grade, setGrade] = useState(1.0);
+    const [avgGrade, setAvgGrade] = useState(0.0);
+    const [listening, setListening] = useState(false);
+
+    Sound.setCategory('Playback');
+    const [sound, setSound] = useState<Sound | null>(null);
 
     function startNextTarget(){
+      // Stop listening
+      setListening(false);
+      setAvgGrade(0.0);
 
-      
+      // Get target (increment)
+
+      // Play sound
+      //TODO refactor this into "pitches" so I should be able to "import" pitches and call playback with a single line.
+      if (sound) {
+        sound.play((success) => {
+          console.log("trying to play");
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+            // Reset the player to the beginning
+            sound.setCurrentTime(0);
+          }
+        });
+      }
+      //wait for note to finish before listening
+      const timerId = setTimeout(() => {
+            evaluate();
+        }, 5000);
     }
 
+    const evaluate = useCallback(() => {
+      //start listening
+      setAvgGrade(0.0);
+      setListening(true);
 
+      //wait 5 seconds then stop listening: grade will happen while listening.
+      const timerId = setTimeout(() => {
+            setListening(false);
+            //determine retry, increment, or decrement. 
+           //what is the score threshold? -- for now I am saying 70%
+           if(avgGrade >=70)
+           {
+             console.log(avgGrade+ " great job you passed. lets increment the pitch!");
+           }else{
+             console.log(avgGrade+" hmm keep working at it!");
+           }
+           }, 3000);
+    }, [avgGrade]);
+
+    const loadSounds = () => {
+      // TODO: all library sounds. from pitches.
+     const sound = new Sound("c4.mp3", Sound.MAIN_BUNDLE, (error: any) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // loaded successfully
+      console.log('duration in seconds: ' + sound.getDuration() + 'number of channels: ' + sound.getNumberOfChannels());
+      sound.setVolume(1.0);
+      // 4. Update state with the loaded Sound object
+      setSound(sound);
+    });
+  };
+
+  const releaseSounds = () => {
+    // TODO all library sounds. from pitches.
+    if (sound) {
+        sound.release();
+        setSound(null);
+      }
+  }
+
+  //playback effect
+  useEffect(() =>{
+    loadSounds();
+    return () => {
+      releaseSounds();
+    }
+  }, []);
+
+  // pitch detection effect
    useEffect(() => {
     let subscription: any; 
   
-    try {
-      PitchDetector.start();
-      subscription = PitchDetector.addListener((value: { frequency: number, tone: string}) => {
-        setHz(value.frequency); // the fqz of what you are singing
-        setNote(value.tone);    // the name of the pitch you are singing. 
-        setGrade(Grade.grade(expected, value.frequency));
+    if(listening == true){
+      try {
+        PitchDetector.start();
+        subscription = PitchDetector.addListener((value: { frequency: number, tone: string}) => {
+          setHz(value.frequency); // the fqz of what you are singing
+          setNote(value.tone);    // the name of the pitch you are singing. 
+          let current = Grade.grade(expected, value.frequency)
+          setAvgGrade((grade + current) / 2);
+          setGrade(current);
 
       });
-      console.log("Pitch detection started.");
-    } catch (e) {
-      console.error("Failed to start pitch detector:", e);
+        console.log("Pitch detection started.");
+      } catch (e) {
+        console.error("Failed to start pitch detector:", e);
+      }
     }
     
     // The cleanup function
@@ -79,7 +161,7 @@ const SetRangeScreen: React.FC<setRangeScreenProps> = ({ onBack }) => {
         console.log("Pitch detection stopped and listener removed.");
       }
     };
-  }, []); 
+  }, [listening]); 
 
   return (
     <SafeAreaView style={{top:25}}>
@@ -101,7 +183,7 @@ const SetRangeScreen: React.FC<setRangeScreenProps> = ({ onBack }) => {
       </View>
       <View style={styles.controls}>
         <Text style={styles.titleText}>
-          Grade: {grade}
+          Grade: {avgGrade}
         </Text>
       </View>
       <TouchableOpacity style={[styles.button]} onPress={startNextTarget}>
